d8648361fa33e8b1ddbe6fc0e37d3c550c61605e
EsbainBello10 <122801516+Esbainbellomarcelino@users.noreply.github.com>
2023-01-24 14:56:54 +0000

    en este programa usamos variables para sumar

---
+++ semana2/programa3.py
@@ -0,0 +1,11 @@
+
+"""
+programa3
+nombre: Esbain Bello
+fecha: 24/01/23
+description: sumar y multiplicar variables 
+"""
+variable1 = 10
+variable2 = 5
+print(variable1+variable2) 
+print (" {}+{}={}". aformat(variable1,variable2)))
