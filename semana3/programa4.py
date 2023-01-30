d82944600e8e54acf83e2cc23657324c51fa7906
EsbainBello10 <122801516+Esbainbellomarcelino@users.noreply.github.com>
2023-01-30 14:57:23 +0000

    ejecutamos el programa 4 para suma, resta, multiplicación, división y potencia

---
+++ semana3/programa4.py
@@ -0,0 +1,21 @@
+
+
+"""
+programa4
+nombre: Esbain Bello
+fecha: 30/01/23
+description:
+"""
+numero1= input("Numero 1: ")
+numero2= 10
+suma=int (numero1) + numero2
+resta= int(numero1) - numero2
+multi = int(numero1)* numero2
+div = int(numero1) / numero2
+
+pote = int(numero1)** numero2
+print(suma)
+print(resta)
+print(multi)
+print(div)
+print(pote)
