9adc5f7e6a5112be04bc5422b7bc8ba0b9531ca6
EsbainBello10 <122801516+Esbainbellomarcelino@users.noreply.github.com>
2023-01-31 13:45:47 +0000

    PERIMETRO Y AREA

---
+++ semana3/programa5.py
@@ -0,0 +1,15 @@
+"""
+programa5
+nombre: Esbain Bello
+fecha: 30/01/23
+description:
+"""
+print("Area del perimetro del triangulo")
+print("Area del triangulo")
+b= int(input("ingrese la base")
+h= int(input("ingrese la altura "))
+l1= int(input("ingrese la media del lado:"))
+A= (b*h)/2
+p=l1*3
+print("perimetro: ", p)
+print("Area: ", A )             
