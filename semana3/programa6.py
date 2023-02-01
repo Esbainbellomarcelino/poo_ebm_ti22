df559cea0c08e2f13274b3e6c1db0b8d2590c9a0
EsbainBello10 <122801516+Esbainbellomarcelino@users.noreply.github.com>
2023-02-01 14:54:36 +0000

    comparar 2 números y saber cual es mayor

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -12,5 +12,5 @@ radio=int(input("ingrese el radio dell circulo"))
 perimetro=int(input("ingrese el perimetro del circulo:"))  
 perimetro = 2*3.14*radio
 area = 3.14* (radio^2)
-print ("el perimetro de un circulo es de ".format(radio, perimetro)
+print ("el perimetro de un circulo es de" (radio,perimetro))
 
---
+++ semana3/programa7.py
@@ -0,0 +1,21 @@
+"""
+programa7
+nombre: Esbain Bello
+fecha: o1/02/23
+description: comparar 2 numeros y saber cual es mayor 
+"""
+
+print("comparar 2 numeros enteros e imprimir el numero mayor ")
+
+numero1= int(input("ingrese un Numero 1: "))
+
+numero2=int (input("ingrese el Numero 2: "))
+
+if   numero1>numero2:
+
+ print("el numero mayor es: ", numero1)
+
+else:
+    print("el numero mayor es: ",numero2)
+
+
